9650dc21937053bf8379c9bb7125030330f0bc47
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2024-08-23 09:11:13 +0000

    [Finish] 2-9 モデル同士の関連付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', to: 'homes#about', as: 'about'
   devise_for :users
   root to: "homes#top"
